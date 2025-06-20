12fe1bfa55b3a67e7387e6c4c1edc5a5c336c278
Richard D <cyberich@northeastern.edu>
2025-06-20 11:10:35 -0400

    Added kaggle_dataset, modified data_generator, test_data_generator to utilize kaggle_data_set, modified gen_data_info.csv to generate different subset dataset, requirements.txt also changed

--- data_generator.py
+++ data_generator.py
@@ -1,37 +1,90 @@
 
+from typing import List, Tuple
 import pandas as pd
+from kaggle_dataset import KaggleDataSet
 
 class DataGenerator:
-    POLARITY_COLUMN_NAME = "Label"
-    TITLE_COLUMN_NAME = "Title"
-    REVIEW_COLUMN_NAME = "Review"
-    POLARITY_VALUE_1 = 1
-    POLARITY_VALUE_2 = 2
-    HALF_SIZE = 2
 
+    HALF_SIZE = 2
+    RANDOMIZE_STATE_VAL = 128
+
+    ### 
+    ### Ensure the GEN_DATA_INFO values match the ones in the gen_data_info.csv file
+    ###
+    GEN_DATA_INFO_FILE_NAME = "gen_data_info.csv"
     GEN_DATA_INFO_DATA_TYPE_COLUMN_NAME = "Data_Type"
     GEN_DATA_INFO_SAMPLE_SIZE_COLUMN_NAME = "Sample_Size"
     GEN_DATA_INFO_OUTPUT_FILE_COLUMN_NAME = "Output_File"
+    GEN_DATA_INFO_DATASET_TYPE_TRAIN_VAL = "train"
+    GEN_DATA_INFO_DATASET_TYPE_TEST_VAL = "test"
+    GEN_DATA_INFO_DATASET_TYPE_VALIDATION_VAL = "validation"
 
-    def __init__(self, train_df: pd.DataFrame, test_df: pd.DataFrame, validation_df: pd.DataFrame):
+
+    def __init__(self, 
+                 kaggle_dataset: KaggleDataSet):
 
         ### Maps data set type to the dataframe variable
         self.dataset_map = {
-            'train': train_df,
-            'test': test_df,
-            'validation': validation_df
+            DataGenerator.GEN_DATA_INFO_DATASET_TYPE_TRAIN_VAL: kaggle_dataset.get_train_df(),
+            DataGenerator.GEN_DATA_INFO_DATASET_TYPE_TEST_VAL: kaggle_dataset.get_test_df(),
+            DataGenerator.GEN_DATA_INFO_DATASET_TYPE_VALIDATION_VAL: kaggle_dataset.get_validation_df()
         }
 
         ### Initialize a dictionary to track used indices for each dataset type
         ### in order to ensure uniqueness
         self.used_indices = {
-            'train': set(),
-            'test': set(),
-            'validation': set()
+            DataGenerator.GEN_DATA_INFO_DATASET_TYPE_TRAIN_VAL: set(),
+            DataGenerator.GEN_DATA_INFO_DATASET_TYPE_TEST_VAL: set(),
+            DataGenerator.GEN_DATA_INFO_DATASET_TYPE_VALIDATION_VAL: set()
         }
 
-    def generate_datasets(self, info_file: str='gen_data_info.csv', ensure_uniqueness: bool=False):
-        # Read the metadata csv file
+
+    def generate_datasets(self, 
+                          info_file: str=None, 
+                          ensure_uniqueness_across_same_dataset_type: bool=True, 
+                          randomize_samples=True) -> List[Tuple[str, bool, bool, pd.DataFrame]]:
+
+        """ generate_datasets downsamples the datasets from source DataFrames based on a configuration CSV.
+            the source DataFrames are passed to the constructor.
+        
+            ensure_uniqueness_across_same_dataset_type: ensures the rows do not overlap across the same dataset type files.
+                    In other words, row 1 will not appear in both train_10K.csv and train_100K.csv
+            
+            randomize_samples: determines whether the row sampling is deterministic or non-deterministic
+                    If set to False, each subsequent call to generate_dataset may not return the same samples.  
+                    If set to True, then the same samples are return after each call.
+            
+            ensure_uniqueness_across_same_dataset_type = True, randomize_samples = False:
+                the behavior is no duplicate rows in the same dataset type files.
+                the file will look the same after repeated calls to generate_dataset
+                use this setting to generate files that don't overlap but deterministic
+            
+            ensure_uniqueness_across_same_dataset_type = True, randomize_samples = True
+                the behavior is no duplicate rows in the same dataset type files.
+                the file will not look the same after repeated calls to generate_dataset
+                use this setting to generate files that don't overlap and non-deterministic
+
+            ensure_uniqueness_across_same_dataset_type = False, randomize_samples = False
+                duplicate rows can appear in the same dataset type files
+                the file will look the same after repeated calls to generate_dataset
+                use this setting to generate files that overlap but deterministic
+            
+            ensure_uniqueness_across_same_dataset_type = False, randomize_samples = True
+                duplicate rows can appear in the same dataset type files
+                the file will not look the same after repeated calls to generate_dataset
+                use this setting for complete randomness
+            
+            return value: list of tuples consisting of 
+                the output file from the entry in the CSV config file,i.e., train_10K.csv or test_100K.csv
+                the value for ensure_uniqueness_across_same_dataset_type,
+                the value for randomize_samples,
+                pd.DataFrame of the combined output for the output file.
+        """
+
+        ### Default to DataGenerator.GEN_DATA_INFO_FILE_NAME if info_file is empty/missing
+        info_file = info_file or DataGenerator.GEN_DATA_INFO_FILE_NAME
+
+        ### Read the metadata csv file
         try:
             info_df = pd.read_csv(info_file, 
                                   header=None, 
@@ -58,56 +111,72 @@ class DataGenerator:
             source_df = self.dataset_map[dataset_type]
             
             # Check label distribution
-            label_counts = source_df[DataGenerator.POLARITY_COLUMN_NAME].value_counts()
+            label_counts = source_df[KaggleDataSet.POLARITY_COLUMN_NAME].value_counts()
             print(f"\nProcessing {output_file} from {dataset_type} dataset")
             print("Label counts:\n", label_counts)
             
             # Ensure enough rows for each label (50% split)
             half_size = sample_size // DataGenerator.HALF_SIZE
             
-            # Filter available rows based on used indices if ensure_unique is True
-            if ensure_uniqueness:
+            # Filter available rows based on used indices if ensure_uniqueness_across_same_dataset_type is True
+            if ensure_uniqueness_across_same_dataset_type:
                 available_df = source_df[~source_df.index.isin(self.used_indices[dataset_type])]
-                label_counts_available = available_df[DataGenerator.POLARITY_COLUMN_NAME].value_counts()
-                if (label_counts_available.get(DataGenerator.POLARITY_VALUE_1, 0) < half_size or 
-                    label_counts_available.get(DataGenerator.POLARITY_VALUE_2, 0) < half_size):
-                    print(f"Error: Not enough unique rows for label {DataGenerator.POLARITY_VALUE_1} or "
-                          f"label {DataGenerator.POLARITY_VALUE_2} to sample {half_size} each in {output_file}. Skipping.")
+                label_counts_available = available_df[KaggleDataSet.POLARITY_COLUMN_NAME].value_counts()
+                if (label_counts_available.get(KaggleDataSet.POLARITY_VALUE_1, 0) < half_size or 
+                    label_counts_available.get(KaggleDataSet.POLARITY_VALUE_2, 0) < half_size):
+                    print(f"Error: Not enough unique rows for label {KaggleDataSet.POLARITY_VALUE_1} or "
+                          f"label {KaggleDataSet.POLARITY_VALUE_2} to sample {half_size} each in {output_file}. Skipping.")
                     continue
             else:
                 available_df = source_df
-                if (label_counts.get(DataGenerator.POLARITY_VALUE_1, 0) < half_size or 
-                    label_counts.get(DataGenerator.POLARITY_VALUE_2, 0) < half_size):
-                    print(f"Error: Not enough rows for label {DataGenerator.POLARITY_VALUE_1} or "
-                          f"label {DataGenerator.POLARITY_VALUE_2} to sample {half_size} each in {output_file}. Skipping.")
+                if (label_counts.get(KaggleDataSet.POLARITY_VALUE_1, 0) < half_size or 
+                    label_counts.get(KaggleDataSet.POLARITY_VALUE_2, 0) < half_size):
+                    print(f"Error: Not enough rows for label {KaggleDataSet.POLARITY_VALUE_1} or "
+                          f"label {KaggleDataSet.POLARITY_VALUE_2} to sample {half_size} each in {output_file}. Skipping.")
                     continue
             
             # Sample half_size rows for each label
-            sample_1 = available_df[available_df[DataGenerator.POLARITY_COLUMN_NAME] == DataGenerator.POLARITY_VALUE_1].sample(
-                n=half_size, random_state=42)
-            sample_2 = available_df[available_df[DataGenerator.POLARITY_COLUMN_NAME] == DataGenerator.POLARITY_VALUE_2].sample(
-                n=half_size, random_state=42)
-            
+            if randomize_samples:
+                ### Shuffle the sampling of rows from each label
+                sample_1 = available_df[available_df[KaggleDataSet.POLARITY_COLUMN_NAME] == KaggleDataSet.POLARITY_VALUE_1].sample(n=half_size)
+                sample_2 = available_df[available_df[KaggleDataSet.POLARITY_COLUMN_NAME] == KaggleDataSet.POLARITY_VALUE_2].sample(n=half_size)
+            else:
+                ### Do not shuffle sample, use the same seed for each call to this method so the row "sampling" from each label is the same
+                sample_1 = available_df[available_df[KaggleDataSet.POLARITY_COLUMN_NAME] == KaggleDataSet.POLARITY_VALUE_1].sample(
+                    n=half_size, random_state=DataGenerator.RANDOMIZE_STATE_VAL)
+                sample_2 = available_df[available_df[KaggleDataSet.POLARITY_COLUMN_NAME] == KaggleDataSet.POLARITY_VALUE_2].sample(
+                    n=half_size, random_state=DataGenerator.RANDOMIZE_STATE_VAL)
+
             # Combine the samples
             downsampled_df = pd.concat([sample_1, sample_2])
             
-            # Shuffle the combined DataFrame
-            downsampled_df = downsampled_df.sample(frac=1, random_state=42).reset_index(drop=True)
+            if randomize_samples:
+                # Shuffle the combined DataFrame
+                ####downsampled_df = downsampled_df.sample(frac=1, random_state=DataGenerator.RANDOMIZE_STATE_VAL).reset_index(drop=True)
+                downsampled_df = downsampled_df.sample(frac=1).reset_index(drop=True)
+
+            else:
+                ### DO not shuffle the data frame, let the ordering of the pd.concat takes care of the ordering which should be deterministic
+                downsampled_df = downsampled_df.reset_index(drop=True)
             
             # Save to CSV file
-            downsampled_df.to_csv(output_file, index=False)
+            downsampled_df.to_csv(output_file, index=False, header=False)
             
             # Update used indices if ensure_unique is True
-            if ensure_uniqueness:
+            if ensure_uniqueness_across_same_dataset_type:
                 self.used_indices[dataset_type].update(sample_1.index)
                 self.used_indices[dataset_type].update(sample_2.index)
             
             # Verify the save
-            print(f"Downsampled dataset saved to '{output_file}'")
-            print("Downsampled dataset label counts:\n", downsampled_df[DataGenerator.POLARITY_COLUMN_NAME].value_counts())
+            print(f"Downsampled dataset saved to '{output_file}', ensure uniqueness across dataset type: {ensure_uniqueness_across_same_dataset_type}, randomize samples: {randomize_samples}")
+            print("Downsampled dataset label counts:\n", downsampled_df[KaggleDataSet.POLARITY_COLUMN_NAME].value_counts())
             print("Downsampled dataset size:", downsampled_df.shape)
             
-            # Store result
-            results.append((output_file, downsampled_df))
+            ##
+            ### Return the generated output file's name which comes from the CSV config file
+            ###        ensure_uniqueness_across_same_dataset_type, randomize_samples,
+            ###        and the combined pd.DataFrame which is writtent to output_file
+            ###
+            results.append((output_file, ensure_uniqueness_across_same_dataset_type, randomize_samples, downsampled_df))
         
         return results

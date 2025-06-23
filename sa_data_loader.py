3c97037fbefdab84c6f6016b9c68f6de9ffcd250
Richard D <cyberich@northeastern.edu>
2025-06-23 13:56:49 -0400

    Added __str__ for pretty printing.

--- sa_data_loader.py
+++ sa_data_loader.py
@@ -13,18 +13,18 @@ class SADataLoader:
         self.test_df = None
         self.validation_df = None
 
+    def __str__(self):
+        return f"Train csv: {self.path_to_train_csv_file}, size: {len(self.get_train_df())}, Test csv: {self.path_to_test_csv_file}, size: {len(self.get_test_df())}, Test csv: {self.path_to_validation_csv_file}, size: {len(self.get_validation_df())}"
+
     def load_data(self, column_names: list[str]):
         print("Path to train csv file:", self.path_to_train_csv_file)
-        print(self.path_to_train_csv_file)
         self.train_df = pd.read_csv(self.path_to_train_csv_file, header=None, names=column_names)
 
         print("Path to test csv file:", self.path_to_test_csv_file)
-        print(self.path_to_test_csv_file)
         self.test_df = pd.read_csv(self.path_to_test_csv_file, header=None, names=column_names)
 
         if self.path_to_validation_csv_file is not None:
             print("Path to validation csv file:", self.path_to_validation_csv_file)
-            print(self.path_to_validation_csv_file)
             self.validation_df = pd.read_csv(self.path_to_validation_csv_file, header=None, names=column_names)
 
         print(f"Train size: {len(self.train_df)}")

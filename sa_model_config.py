e92b3f21cc21a4dc26d996cc69e66051305d5a97
Richard D <cyberich@northeastern.edu>
2025-06-24 16:39:06 -0400

    Added static class methods to access the various column names of the model config file such as SAModelConfig.model_config_model_name() and reduced the scope of the class constant that used to provide similar access.  Added verify_model_params() to provide a way for models to check if the model param keys they are using are actually defined in the model config file.

--- sa_model_config.py
+++ sa_model_config.py
@@ -1,16 +1,68 @@
 
-from typing import Dict
+from typing import Any, Dict, List
 
 class SAModelConfig:
 
-    DEFAULT_MODEL_CONFIG_FILE_NAME = "model_config.csv"
-    MODEL_CONFIG_MODEL_NAME = "Model Name"
-    MODEL_CONFIG_MODULE_NAME = "Model Module Name"
-    MODEL_CONFIG_CLASS_NAME = "Model Class Name"
-    MODEL_CONFIG_TRAIN_CSV = "Train CSV"
-    MODEL_CONFIG_TEST_CSV = "Test CSV"
-    MODEL_CONFIG_VALIDATION_CSV = "Validation CSV"
-    MODEL_CONFIG_MODEL_PARAMS = "Model Params"
+    _DEFAULT_MODEL_CONFIG_FILE_NAME = "model_config.csv"
+    _MODEL_CONFIG_MODEL_NAME = "Model Name"
+    _MODEL_CONFIG_MODULE_NAME = "Model Module Name"
+    _MODEL_CONFIG_CLASS_NAME = "Model Class Name"
+    _MODEL_CONFIG_TRAIN_CSV = "Train CSV"
+    _MODEL_CONFIG_TEST_CSV = "Test CSV"
+    _MODEL_CONFIG_VALIDATION_CSV = "Validation CSV"
+    _MODEL_CONFIG_MODEL_PARAMS = "Model Params"
+
+    ###
+    ### Order of the columns in the model_config.csv file where the
+    ### 1st column is the model name
+    ### 2nd column is the model module name
+    ### 3rd column is the model class name
+    ### .....
+    ### last column is the model params
+
+    _MODEL_CONFIG_COLUMN_NAMES = [_MODEL_CONFIG_MODEL_NAME, 
+                                  _MODEL_CONFIG_MODULE_NAME, 
+                                  _MODEL_CONFIG_CLASS_NAME, 
+                                  _MODEL_CONFIG_TRAIN_CSV, 
+                                  _MODEL_CONFIG_TEST_CSV, 
+                                  _MODEL_CONFIG_VALIDATION_CSV, 
+                                  _MODEL_CONFIG_MODEL_PARAMS]
+
+    @staticmethod
+    def get_default_model_config_file_name():
+        return SAModelConfig._DEFAULT_MODEL_CONFIG_FILE_NAME 
+    
+    @staticmethod
+    def get_model_config_column_names():
+        return SAModelConfig._MODEL_CONFIG_COLUMN_NAMES.copy()  # Return a shallow copy    
+
+    @staticmethod
+    def get_model_config_model_name():
+        return SAModelConfig._MODEL_CONFIG_MODEL_NAME 
+    
+    @staticmethod
+    def get_model_config_model_module_name():
+        return SAModelConfig._MODEL_CONFIG_MODULE_NAME 
+
+    @staticmethod
+    def get_model_config_model_class_name():
+        return SAModelConfig._MODEL_CONFIG_CLASS_NAME 
+
+    @staticmethod
+    def get_model_config_model_train_csv_name():
+        return SAModelConfig._MODEL_CONFIG_TRAIN_CSV 
+
+    @staticmethod
+    def get_model_config_model_test_csv_name():
+        return SAModelConfig._MODEL_CONFIG_TEST_CSV 
+
+    @staticmethod
+    def get_model_config_model_validation_csv_name():
+        return SAModelConfig._MODEL_CONFIG_VALIDATION_CSV 
+
+    @staticmethod
+    def get_model_config_model_params():
+        return SAModelConfig._MODEL_CONFIG_MODEL_PARAMS 
 
     def __init__(self, 
                  model_name: str=None, 
@@ -19,7 +71,7 @@ class SAModelConfig:
                  model_train_csv_file_name: str=None,
                  model_test_csv_file_name: str=None,
                  model_validation_csv_file_name: str=None,
-                 model_params: Dict=None):
+                 model_params: Dict[str, Any]=None):
         self.model_name = model_name
         self.model_module_name = model_module_name
         self.model_class_name = model_class_name
@@ -27,7 +79,7 @@ class SAModelConfig:
         self.model_test_csv_file_name = model_test_csv_file_name
         self.model_validation_csv_file_name = model_validation_csv_file_name
         self.model_params = model_params
-    
+
     def __str__(self):
         return f"{self.get_model_name()}/{self.get_model_module_name()}/{self.get_model_class_name()}/{self.get_model_train_csv_file_name()}/{self.get_model_test_csv_file_name()}/{self.get_model_validation_csv_file_name()}/{self.get_model_params()}"
     
@@ -49,5 +101,11 @@ class SAModelConfig:
     def get_model_validation_csv_file_name(self) -> str:
         return self.model_validation_csv_file_name
     
-    def get_model_params(self) -> Dict:
+    def get_model_params(self) -> Dict[str, Any]:
         return self.model_params
+
+    def verify_model_params(self, model_params_to_check:List[str]) -> bool:    
+        missing = [key for key in model_params_to_check if key not in self.model_params]
+        if missing:
+            raise ValueError(f"Keys not found in model params: {missing}")
+        return True

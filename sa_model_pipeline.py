e84834a0473da48442836b4692b0be85b90810d7
Richard D <cyberich@northeastern.edu>
2025-06-24 16:48:10 -0400

    Uses SASentinmentModel's run() method instead of calling each of the method individually in _run_single_model_pipeline_with_model_params.  Also changed to use KaggleDataSet's static class method instead of the static class constants to access Kaggle dataset's column names.

--- sa_model_pipeline.py
+++ sa_model_pipeline.py
@@ -30,9 +30,6 @@ class SAModelPipeline:
     def _load_model_params(self):
         self.map_of_model_params = {}
         model_config_len = len(self.list_of_model_config)
-        column_names = [KaggleDataSet.POLARITY_COLUMN_NAME, 
-                        KaggleDataSet.TITLE_COLUMN_NAME,
-                        KaggleDataSet.REVIEW_COLUMN_NAME]
 
         cwd = os.getcwd()
         for i in range(model_config_len):
@@ -47,10 +44,10 @@ class SAModelPipeline:
             path_to_validation_csv_file = os.path.join(cwd, validation_file)
 
             sa_data_loader = SADataLoader(path_to_train_csv_file, path_to_test_csv_file, path_to_validation_csv_file)
-            sa_data_loader.load_data(column_names)
+            sa_data_loader.load_data(KaggleDataSet.get_kaggle_column_names())
 
             sa_model_params = SAModelParams(sa_data_loader, model_config)
-            self.map_of_model_params[self._model_lookup_key(model_config.get_model_module_name() ,model_config.get_model_class_name())] = sa_model_params
+            self.map_of_model_params[self._model_lookup_key(model_config.get_model_module_name(), model_config.get_model_class_name())] = sa_model_params
 
     def _instantiate_models(self):
         model_config_len = len(self.list_of_model_config)
@@ -76,40 +73,34 @@ class SAModelPipeline:
                 logger.info(f"Model obtained: {model}")
                 
                 self.map_of_instantiated_models[self._model_lookup_key(module_name, class_name)] = model
-                logger.info(f"Loaded model: {module_name} : {class_name}: {sa_model_params}")
+                logger.info(f"Model loaded: {module_name} : {class_name}: {sa_model_params}")
             except Exception as e:
-                logger.error(f"Failed to load model {class_name}: {str(e)}")
+                logger.error(f"Failed to load model {module_name} : {class_name}: {str(e)}")
             
             logger.info("="*100)
 
-    
-
     def run_model_pipeline(self):
         for key, model in self.map_of_instantiated_models.items():
-            logger.info(f"Running model: {model.__class__.__name__}")
             sa_model_params = self.map_of_model_params.get(key)
             self._run_single_model_pipeline_with_model_params(model, sa_model_params)
-
 
     def run_single_model_pipeline(self, 
                                   model_module_name:str=None,
                                   model_class_name:str=None):
         
             model = self.map_of_instantiated_models.get(self._model_lookup_key(model_module_name, model_class_name))
-            logger.info(f"Running model: {model.__class__.__name__}")
             sa_model_params = self.map_of_model_params.get(self._model_lookup_key(model_module_name, model_class_name))
             self._run_single_model_pipeline_with_model_params(model, sa_model_params)
-
     
     def _run_single_model_pipeline_with_model_params (self, 
                                                       sa_sentiment_model: SASentimentModel=None,
                                                       sa_model_params: SAModelParams=None):
         try:
-            logger.info(sa_sentiment_model.register(sa_model_params))
-            sa_sentiment_model.preprocess(sa_model_params)
-            sa_sentiment_model.fit(sa_model_params)
-            sa_sentiment_model.predict(sa_model_params)
-            sa_sentiment_model.evaluate(sa_model_params)
+            logger.info(f"Running model: {sa_sentiment_model.__class__.__name__}")
+
+            ### Call the model's run method which will run the model pipeline
+            sa_sentiment_model.run(sa_model_params)
+
             logger.info(f"Finished running model: {sa_sentiment_model.__class__.__name__}")
         except Exception as e:
             logger.error(f"Error running model: {sa_sentiment_model.__class__.__name__}: {str(e)}")

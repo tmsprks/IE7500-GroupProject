caf93f278ee151e8d09be4560e40d3a9da76f7a8
Richard D <cyberich@northeastern.edu>
2025-06-24 16:35:40 -0400

    Minor changes to method signature to enhance type hinting.

--- sa_data_factory.py
+++ sa_data_factory.py
@@ -16,22 +16,25 @@ class SADataFactory:
         self.train_df = None
         self.validation_df = None
 
+        ###
+        ### Perform the explicit train/validation split since the Kaggle dataset doesn't
+        ### have a validation csv file, only the train and test csv file
+        ###
         self._train_validation_split(kaggle_dataset, train_test_split_ratio)
-
-    
 
     def _train_validation_split(self, 
                                 kaggle_dataset: KaggleDataSet, 
                                 train_test_split_ratio:float):
-        self.train_df, self.validation_df = train_test_split(kaggle_dataset.get_train_df(), test_size=train_test_split_ratio)
+        self.train_df, self.validation_df = train_test_split(kaggle_dataset.get_train_df(), 
+                                                             test_size=train_test_split_ratio)
 
-    def get_train_df(self):
+    def get_train_df(self) -> pd.DataFrame:
         return self.train_df
     
-    def get_test_df(self):
+    def get_test_df(self) -> pd.DataFrame:
         return self.kaggle_dataset.get_test_df()
     
-    def get_validation_df(self):
+    def get_validation_df(self) -> pd.DataFrame:
         return self.validation_df
 
 
